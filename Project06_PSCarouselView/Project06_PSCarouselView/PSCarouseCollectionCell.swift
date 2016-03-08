dad41744a41e9cb9a2b02f7950a025e53673ee8f
jiangchao <jiangchaobj@yahoo.com>
2016-03-08 23:02:48 +0800

    Swift Tour Playground file

---
+++ Project06_PSCarouselView/Project06_PSCarouselView/PSCarouseCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  PSCarouseCollectionCell.swift
+//  Project06_PSCarouselView
+//
+//  Created by jiangchao on 16/3/3.
+//  Copyright © 2016年 jiangchao. All rights reserved.
+//
+
+import UIKit
+
+class PSCarouseCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Project06_PSCarouselView/Project06_PSCarouselView/PSCarouselView.swift
@@ -0,0 +1,21 @@
+//
+//  PSCarouselView.swift
+//  Project06_PSCarouselView
+//
+//  Created by jiangchao on 16/3/3.
+//  Copyright © 2016年 jiangchao. All rights reserved.
+//
+
+import UIKit
+
+class PSCarouselView: UICollectionView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
